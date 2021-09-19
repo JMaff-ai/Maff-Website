321fbebb441b6f906be618666c9cb1ef9be16812
JMaff-ai <jmaffettone8802@gmail.com>
2021-09-18 23:14:52 -0400

    1 file modified, 1 added, 1 deleted

---
+++ src/MenuItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MenuItem(props) {
+  return '';
+}
+
+export default MenuItem;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+import MenuItem from './MenuItem';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+let name = 'Soft served Ice Cream';
+let price = 2.00
+let description = 'a soft served ice cream in bowl with popular flavors of Vanilla, Choclate or Strawberry.';
+
+const menuItemElement1 = (
+  <div>
+    <h2>
+      {name}, ${price}
+    </h2>
+    <p>{description}</p>
+  </div>
+
+);
+
+ name = 'Tartufo';
+ price = 3.50
+ description = 'choclate and vanilla ice cream with a cherry in the middle smolthered in rich choclate for a choclate casing.'
+
+const menuItemElement2 = (
+  <div>
+    <h2>
+      {name}, ${price}
+    </h2>
+    <p>{description}</p>
+  </div>
+
+);
+
+
+const menuElement = (
+  <div>
+    {menuItemElement1}
+    {menuItemElement2}
+  </div>
+
+);
+
+
+ReactDOM.render(menuElement, document.getElementById("root"));
