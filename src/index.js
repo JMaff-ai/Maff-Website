e8f5a988294ff5529ec44322ecfdae6fa72bd6ec
JMaff-ai <jmaffettone8802@gmail.com>
2021-09-19 11:19:07 -0400

    1 file modified

--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ import ReactDOM from "react-dom";
 
 import MenuItem from './MenuItem';
 
+<html>
+  <head>
+    <title>Maff's Dessert Bar</title>
+  </head>
+  <body>
+    <h1>Welcome to Maff's dessert bar</h1>
+    <p>
+      We are currently looking for more employees to help out around the bar with serving food and cleaning tables, and we have fun board games for not only the adults but the kids and employees as well like <em>Hungry Hungry Hippos</em> Wifi is free for both employees and quests.
+    </p>
+    <h2>Directions</h2>
+    <p>
+      We are locatied in On Sunrise Highway near Bellmore, right around Home Depot. Come enjoy desserts or start working today!
+    </p>
+    <h3>Menu</h3>
+    <p>
+      Below is a sample of what we serve, we got varity of cakes and some italian delicaties. Enjoy them however you like!
+    </p>
+  </body>
+</html>
+
 let name = 'Soft served Ice Cream';
 let price = 2.00
 let description = 'a soft served ice cream in bowl with popular flavors of Vanilla, Choclate or Strawberry.';
