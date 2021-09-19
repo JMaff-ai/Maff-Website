22a5588dfb85a67878d11ebcbec64f2abf41d16d
JMaff-ai <jmaffettone8802@gmail.com>
2021-09-18 23:41:36 -0400

    1 file modified

--- src/MenuItem.js
+++ src/MenuItem.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function MenuItem(props) {
-  return '';
+  return (
+    <div>
+      <h3>
+        {props.name}, ${props.price}
+      </h3>
+      <p>{props.children}</p>
+    </div>
+  )
 }
 
 export default MenuItem;
