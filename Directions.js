d3c8daf72f424f062c900213e81bc5fccafe40d5
JMaff-ai <jmaffettone8802@gmail.com>
2021-09-25 19:21:12 -0400

    1 file modified

--- Directions.js
+++ Directions.js
@@ -4,4 +4,14 @@
     </head>
     <body>
       <h1>Directions</h1>
-      <p>
+      <p>Take the 290 N exit to Seaford - go 0.8 mi</p>
+      <p>Continue on 290 N - go 3.6 mi</p>
+      <p>Exit through 17S - go 0.2 mi</p>
+      <p>In 3 blocks, turn left to Hober Lane - go 1.2 mi</p>
+      <p>In 8 blocks, turn right towards Sunrise Avenue - go 2.6 mi</p>
+      <p>In 2 blocks, turn right towards Hill Highway - go 0.9 mi</p>
+      <p> Continue on Hill Highway - go 1.4 mi</p>
+      <p> Make a right turn towards Gecka Town - go 0.3</p>
+      <p> Make a left at the Geico Building - go 0.4</p>
+      </body>
+</html>
