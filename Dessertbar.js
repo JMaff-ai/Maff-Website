de9055989ede971e6b231b6d69cef7be6444dc84
JMaff-ai <jmaffettone8802@gmail.com>
2021-09-25 18:39:52 -0400

    1 file added

---
+++ Dessertbar.js
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <title>Maff's Dessert Bar</title>
+  </head>
+  <body>
+    <h1>Welcome to Maff's dessert bar</h1>
+    <p>
+      We are currently looking for more employees to help out around the bar with serving food and cleaning tables, and we have fun board games for not only the adults but the kids and employees as well like <em>Hungry Hungry Hippos</em> Wifi is free for both employees and quests.
+    </p>
+    <h2>Directions</h2>
+    <p>
+      We are locatied in On Sunrise Highway near Bellmore, right around Home Depot. Come enjoy desserts or start working today!
+    </p>
+    <h3>Menu</h3>
+    <p>
+      Below is a sample of what we serve, we got varity of cakes and some italian delicaties. Enjoy them however you like!
+    </p>
+  </body>
+</html>
