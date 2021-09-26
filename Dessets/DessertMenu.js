e7a64116119c03277f013766fbfc89525264915e
JMaff-ai <jmaffettone8802@gmail.com>
2021-09-25 20:57:17 -0400

    1 file modified

--- Dessets/DessertMenu.js
+++ Dessets/DessertMenu.js
@@ -5,4 +5,17 @@ function DessertMenu () {
      <p>a soft served ice cream in bowl with popular flavors of Vanilla, Choclate or Strawberry.
       <img src = "images/Soft served Ice cream.jpg"/>
       </p>
-      <p>
+      <p>choclate and vanilla ice cream with a cherry in the middle smolthered in rich choclate for a choclate casing. 
+        <img src ="images/tartufo.jpg"/>
+      </p>
+      <p>A fluffy red velvet cake with vanilla frosting and rainbow sprinkles.
+        <img src ="images/Red Velet Ckae.jpg"/>
+      </p>
+      <p>a platter full of many different cheese ckae flavors. 
+        <img src ="images/Assorted cheesecake.jpg"/>
+      </p>
+    </div>
+  );
+  }
+
+export default DessertMenu();
