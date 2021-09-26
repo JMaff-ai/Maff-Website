8bc8bf57223750341758c557b8a0f4e92c519fd7
JMaff-ai <jmaffettone8802@gmail.com>
2021-09-25 23:48:05 -0400

    1 file modified

--- App.js
+++ App.js
@@ -8,10 +8,12 @@ export default function App() {
   return (
     <Router>
       <Switch>
+      <Route exact path="/DessertBar.js" component={DessertBar} />
       <Route exact path="/about/directions.js">
-      <DessertBar />
-      <DessertMenu />
       <Directions /> (additional HTML/JSX can be added here) </Route>
+      <Route exact path="/Dessets/DessertMenu.js">
+        <DessertMenu />
+      </Route>
 
       </Switch>
     </Router>
