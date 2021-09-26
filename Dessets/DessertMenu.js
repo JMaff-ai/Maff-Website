05924d1dc54077b36016a5231649e8bc0d6b3098
JMaff-ai <jmaffettone8802@gmail.com>
2021-09-25 22:14:01 -0400

    1 file modified

--- Dessets/DessertMenu.js
+++ Dessets/DessertMenu.js
@@ -1,9 +1,17 @@
+import tartufo from "../images/tartufo.jpg";
+import SoftservedIceCream from "../images/Soft served Ice cream.jpg";
+import RedVeletCkae from "../images/Red Velet Ckae.jpg";
+import Assortedcheesecake from "../images/Assorted cheesecake.jpg";
+
+
+
+
 function DessertMenu () {
   return (
     <div>
       <h1>DessertMenu</h1>
      <p>a soft served ice cream in bowl with popular flavors of Vanilla, Choclate or Strawberry.
-      <img src = "images/Soft served Ice cream.jpg"/>
+      <img src = {SoftservedIceCream}/>
       </p>
       <p>choclate and vanilla ice cream with a cherry in the middle smolthered in rich choclate for a choclate casing. 
         <img src ="images/tartufo.jpg"/>
