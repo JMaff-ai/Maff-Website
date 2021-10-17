dfd5abd6842adc319080644a8977566ec8d6ac49
JMaff-ai <jmaffettone8802@gmail.com>
2021-10-16 22:22:35 -0400

    1 file modified

--- src/ImageFigure.js
+++ src/ImageFigure.js
@@ -4,6 +4,10 @@ function ImageFigure(props) {
       <img src={props.imgsrc}
       alt={props.alt}
       width={props.width}
-      
+      height={props.height} />
+  <figcaption>{props.figcaption}</figcaption>
+  </figure>
   )
 }
+
+export default ImageFigure;
