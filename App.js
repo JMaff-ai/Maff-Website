1fd9a85613f8e55a09a79b8df04fb7ba114e0c02
JMaff-ai <jmaffettone8802@gmail.com>
2021-10-16 22:41:10 -0400

    1 file modified

--- App.js
+++ App.js
@@ -3,6 +3,11 @@ import{BrowserRouterasRouter,Switch,Route}from"react-router-dom";
 import DessertBar from "./src/Dessertbar.js";
 import DessertMenu from "./Dessets/DessertMenu.js";
 import Directions from"./Dessertbar/about/Directions.js";
+import ImageFigure from "./src/ImageFigure.js";
+import assortedcheesecake from "./images/Assorted cheesecake.jpg";
+import redveletcake from "./images/Red Velet Ckae.jpg";
+import softservedicecream from "./images/Soft served Ice cream.jpg";
+import tartufo from "./images/tartufo.jpg";
 
 export default function App() {
   return (
@@ -13,6 +18,22 @@ export default function App() {
       <Directions /> (additional HTML/JSX can be added here) </Route>
       <Route exact path="/Dessets/DessertMenu.js">
         <DessertMenu />
+      </Route>
+      <Route exact path="/html/assortedcheesecake.html">
+        <ImageFigure
+        imgsrc={assortedcheesecake}
+        width={300}
+        height={150}
+        alt="Assorted Cheese Cakes"
+        />
+      </Route>
+      <Route exact path="/html/redveletcake.html">
+        <ImageFigure
+        imgsrc={redveletcake}
+        width={300}
+        height={150}
+        alt="Red Velvet Cake"
+        />
       </Route>
       <Route path="/"><h1>Error 404: File Not Found </h1></Route>
 
