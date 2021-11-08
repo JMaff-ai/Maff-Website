0a8e2806c57de9a591cf8a04cd961739c5f991f7
JMaff-ai <jmaffettone8802@gmail.com>
2021-11-07 19:29:08 -0500

    1 file modified

--- Dessertbar.js
+++ Dessertbar.js
@@ -4,6 +4,7 @@ function Dessertbar() {
    return(
    <div>
     <title>Maff's Dessert Bar</title>
+    <link type="text/css" rel="stylesheet" href="Dessertbar.css" />
     <h1>Welcome to Maff's dessert bar</h1>
     <p>
       We are currently looking for more employees to help out around the bar with serving food and cleaning tables, and we have fun board games for not only the adults but the kids and employees as well like <em>Hungry Hungry Hippos</em> Wifi is free for both employees and quests.
